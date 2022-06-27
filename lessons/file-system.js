476952f01859d85693fba83f70b3118ceb3d52a3
Nikita Zelenkov <zelenkov4462@gmail.com>
2022-06-27 13:33:05 +0700

    fs

---
+++ lessons/file-system.js
@@ -0,0 +1,184 @@
+// Файловая система - глобальный объект 'fs'
+const fs = require("fs");
+const path = require("path");
+
+//...Работаем с синхронными операциями
+
+// // создание папки в проекте
+// fs.mkdirSync(path.resolve(__dirname, "dir"));
+
+// создание пложенных папок
+// fs.mkdirSync(path.resolve(__dirname, "dir", "dir2", "dir3"), {
+//   recursive: true,
+// });
+
+//.... Работаем с асинхронным кодом
+
+// Создание вложенных папок
+// console.log("start");
+// fs.mkdir(path.resolve(__dirname, "first"), (err) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log("Create directory");
+// });
+// console.log("end");
+//
+//
+// Удаление папок
+// Удалили папку созданную выше
+// fs.rmdir(path.resolve(__dirname, "first"), (err) => {
+//   if (err) {
+//     throw err;
+//   }
+// });
+//
+//
+//Создать файл и записать в него данные - writeFile
+// Если в файле были данные - то они перезатрутся новыми данными
+//
+// fs.writeFile(
+//   path.resolve(__dirname, "test.txt"),
+//   "5 dfaf as d0 sd52 df",
+//   (err) => {
+//     if (err) {
+//       throw err;
+//     }
+//     console.log("файл записан");
+//   }
+// );
+
+// Если нужно дозаписать данные в файл use функцию - appendFile
+
+// fs.appendFile(
+//   path.resolve(__dirname, "test.txt"),
+//   "Добавили в конец",
+//   (err) => {
+//     if (err) {
+//       throw err;
+//     }
+//     console.log("Файл записан");
+//
+//   }
+// );
+
+// и т.к. appendFile и writeFile - асинхронные, то не понятно
+// какая функция добавится в конец -
+// - нужно вложить одну функцию в другую
+// fs.writeFile(
+//   path.resolve(__dirname, "test.txt"),
+//   "5 dfaf as d0 sd52 df",
+//   (err) => {
+//     if (err) {
+//       throw err;
+//     }
+//     console.log("файл записан");
+//     fs.appendFile(
+//       path.resolve(__dirname, "test.txt"),
+//       "Добавили в конец текст",
+//       (err) => {
+//         if (err) {
+//           throw err;
+//         }
+//         console.log("Файл записан");
+//       }
+//     );
+//   }
+// );
+//
+//
+//
+//В примере выше можно наблюдать "адд колбэков" -
+// когда функции вложены друг в друга -
+//поддерживать данный код очень трудно -
+//
+//В качестве решения используют - ПРОМИСЫ
+//
+//Записываем данные на диск, но use PROMISE
+// const writeFileAsync = async () => {
+//   return new Promise((resolve, reject) =>
+//     fs.writeFile(path.resolve(__dirname, "test.txt"), (err) => {
+//       if (err) {
+//         return reject(err.message);
+//       }
+//       resolve();
+//     })
+//   );
+// };
+
+//Немного улучшаем код - делаем его переиспользуемым -
+// в качестве аргументов передаем путь и данные для записи
+const writeFileAsync = async (path, data) => {
+  return new Promise((resolve, reject) =>
+    fs.writeFile(path, data, (err) => {
+      if (err) {
+        return reject(err.message);
+      }
+      resolve();
+    })
+  );
+};
+//И функция для добавления данных
+const appendFileAsync = async (path, data) => {
+  return new Promise((resolve, reject) =>
+    fs.appendFile(path, data, (err) => {
+      if (err) {
+        return reject(err.message);
+      }
+      resolve();
+    })
+  );
+};
+//
+// writeFileAsync(path.resolve(__dirname, "test.txt"), "data")
+//   .then(() => appendFileAsync(path.resolve(__dirname, "test.txt"), "123"))
+//   .then(() => appendFileAsync(path.resolve(__dirname, "test.txt"), "456"))
+//   .then(() => appendFileAsync(path.resolve(__dirname, "test.txt"), "7899"))
+//   .catch((err) => console.log(err));
+//
+//
+// СЧИТЫВАЕМ данные из файла
+const readFileAsync = async (path) => {
+  return new Promise((resolve, reject) =>
+    fs.readFile(path, { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        return reject(err.message);
+      }
+      resolve(data);
+    })
+  );
+};
+
+// writeFileAsync(path.resolve(__dirname, "test.txt"), "data")
+//   .then(() => appendFileAsync(path.resolve(__dirname, "test.txt"), "123"))
+//   .then(() => appendFileAsync(path.resolve(__dirname, "test.txt"), "456"))
+//   .then(() => appendFileAsync(path.resolve(__dirname, "test.txt"), "7899"))
+//   .then(() => readFileAsync(path.resolve(__dirname, "test.txt")))
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+//
+//
+//Удаление файла с диска
+const removeFileAsync = async (path) => {
+  return new Promise((resolve, reject) =>
+    fs.rm(path, (err) => {
+      if (err) {
+        return reject(err.message);
+      }
+      resolve();
+    })
+  );
+};
+//
+// removeFileAsync(path.resolve(__dirname, "test.txt")).then(() =>
+//   console.log("file was removed")
+// );
+//
+//
+//Задача -
+// Через переменную окружения мы передаем строку,
+// записываем ее в файл, потом этот файл мы считываем,
+// считаем количество слов в этом файле и
+// записываем их уже в другой файл,
+// при этом первый файл мы удаляем
--- lessons/path.js
+++ lessons/path.js
@@ -5,7 +5,14 @@ const path = require("path");
 //   path.join(__dirname, "first", "second")
 // );
 
-// console.log(path.join("Получить путь от корня приложения до текущего файла",__dirname));
+//  D:\Projects\NodeJS\lessons\first\second
+
+// console.log(
+//   path.join("Получить путь от корня приложения до текущего файла", __dirname)
+// );
+
+// \D:\Projects\NodeJS\lessons
+
 // console.log(path.join("Подняться на 2 уровня вверх в адресе",__dirname, "..", ".."));
 
 // console.log(
@@ -13,21 +20,51 @@ const path = require("path");
 //   path.resolve("first", "second", "third")
 // );
 
+// D:\Projects\NodeJS\lessons\first\second\third
+
 const fullpath = path.resolve(__dirname, "first", "second.js");
+
 // console.log("Парсинг пути", path.parse(fullpath));
 
+// Парсинг пути {
+// 	root: 'D:\\',
+// 		dir: 'D:\\Projects\\NodeJS\\lessons\\first',
+// 		base: 'second.js',
+// 		ext: '.js',
+// 		name: 'second'
+// }
+
 // console.log("Разделить (в какую сторону слеш) в ОС", path.sep);
+// Разделить (в какую сторону слеш) в ОС \
 
 // console.log("Проверка на абсолютный путь", path.isAbsolute("first/second"));
+// Проверка на абсолютный путь false
 
 // console.log("Название файла", path.basename(fullpath));
+// Название файла second.js
 
 // console.log("Расширение файла", path.extname(fullpath));
+// Расширение файла .js
 
 // -----------------------------------------------------
 
-const siteURL = "http://localhost:8000/users?id=5123";
+// const siteURL = "http://localhost:8000/users?id=5123";
+//
+// const url = new URL(siteURL);
+//
+// console.log(url);
 
-const url = new URL(siteURL);
-
-console.log(url);
+// URL {
+// 	href: 'http://localhost:8000/users?id=5123',
+// 		origin: 'http://localhost:8000',
+// 		protocol: 'http:',
+// 		username: '',
+// 		password: '',
+// 		host: 'localhost:8000',
+// 		hostname: 'localhost',
+// 		port: '8000',
+// 		pathname: '/users',
+// 		search: '?id=5123',
+// 		searchParams: URLSearchParams { 'id' => '5123' },
+// 	hash: ''
+// }
