cc260b217310584ea7479c558e5be838b6aeca02
Nikita Zelenkov <zelenkov4462@gmail.com>
2022-06-28 10:38:44 +0700

    framework

--- index.js
+++ index.js
@@ -7,7 +7,8 @@
 const http = require("http");
 const EventEmitter = require("events");
 const PORT = process.env.PORT || 5000;
-
+const Router = require("./framework/Router");
+const Application = require("./framework/Application");
 const emitter = new EventEmitter();
 
 //ServerSideRendering - мы на стороне сервера
@@ -45,40 +46,7 @@ const emitter = new EventEmitter();
 // Но писать сервера через if else (как выше в примере) не удобно,
 // пожтому используют фреймворки - типа Express
 //
-class Router {
-  constructor() {
-    this.endpoints = {};
-  }
-
-  request(method = "GET", path, handler) {
-    if (!this.endpoints[path]) {
-      this.endpoints[path] = {};
-    }
-    const endpoint = this.endpoints[path];
-
-    if (endpoint[method]) {
-      throw new Error(`[${method}] по адресу ${path} уже существует`);
-    }
-
-    endpoint[method] = handler;
-
-    emitter.on(`[${path}]:[${method}]`, (req, res) => {
-      handler(req, res);
-    });
-  }
-  get(path, handler) {
-    this.request("GET", path, handler);
-  }
-  post(path, handler) {
-    this.request("POST", path, handler);
-  }
-  put(path, handler) {
-    this.request("PUT", path, handler);
-  }
-  delete(path, handler) {
-    this.request("DELETE", path, handler);
-  }
-}
+const app = new Application();
 
 const router = new Router();
 
@@ -89,12 +57,6 @@ router.get("/posts", (req, res) => {
   res.end("YOU SEND REQUEST TO /POSTS");
 });
 
-const server = http.createServer((req, res) => {
-  const emitted = emitter.emit(`[${req.url}]:[${req.method}]`, req, res);
-  if (!emitted) {
-    res.end();
-  }
-  // res.end(req.url);
-});
+app.addRouter(router);
 
-server.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
